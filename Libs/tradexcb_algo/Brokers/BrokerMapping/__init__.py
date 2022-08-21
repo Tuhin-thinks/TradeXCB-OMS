bce9458b174680fd837974b1c61d86eae9235ef0
tuhin-thinks <tuhinmitra190221@gmail.com>
2022-08-21 11:49:32 +0530

    removed unsupported Brokers structure

--- Libs/tradexcb_algo/Brokers/BrokerMapping/__init__.py
+++
@@ -1,8 +0,0 @@
-import brokers
-
-
-def create_new_broker_instance(**broker_creds):
-    """
-    Do the instance creation and return the specific broker's instance
-    """
-    return brokers.BROKER_MODULES[broker_creds['Stock Broker Name'].lower()](**broker_creds)
